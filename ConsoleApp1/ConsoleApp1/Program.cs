ba47ab46242b1a63f17ba3d508de7297111f804b
tutkutektas <56909628+tutkutektas@users.noreply.github.com>
2020-02-05 11:52:35 +0300

    Selenium Kuruldu

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("sacscasd");
+            IWebDriver driver = new FirefoxDriver();
+            string link = @"https://www.n11.com/";
+            driver.Navigate().GoToUrl(link);
 
         }
     }
